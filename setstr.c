4d901de35d1cd30b81158e8327de07649e23c8b5
Daniel Roethlisberger <daniel@roe.ch>
2018-08-26 18:18:12 +0200

    Fix one-time memory leaks in initialization of setstr_t

--- setstr.c
+++ setstr.c
@@ -20,43 +20,56 @@
 #include <string.h>
 #include <stdio.h>
 
-typedef struct setstr_node {
+typedef struct setstr_obj {
 	tommy_hashtable_node h_node;
 	char *str;
-} setstr_node_t;
+} setstr_obj_t;
 
 static int
-compfunc(const void *str, const void *obj) {
-	const setstr_node_t *node = obj;
-	return strcmp(node->str, str);
+setstr_obj_cmp(const void *str, const void *vobj) {
+	const setstr_obj_t *obj = vobj;
+	return strcmp(obj->str, str);
+}
+
+static void
+setstr_obj_free(void *vobj) {
+	setstr_obj_t *obj = vobj;
+	free(obj->str);
+	free(obj);
 }
 
 /*
- * strings may be NULL only if buckets is 0.
+ * strings may be (and must be) NULL if buckets is 0.
  * Guarantees to deep free strings even on errors.
  */
 int
 setstr_init(setstr_t *this, size_t buckets, char **strings) {
 	this->bucket_max = bucket_max_for_buckets(buckets);
 	this->size = buckets;
+	if (buckets == 0) {
+		assert(this->bucket_max == 0);
+		assert(strings == NULL);
+		return 0;
+	}
+
 	tommy_hashtable_init(&this->hashtable, this->bucket_max);
 
 	for (size_t i = 0; i < buckets; i++) {
-		setstr_node_t *node;
+		setstr_obj_t *obj;
 		tommy_hash_t h;
 
 		if (setstr_contains(this, strings[i])) {
 			free(strings[i]);
 			continue;
 		}
-		node = malloc(sizeof(setstr_node_t));
-		if (!node)
+		obj = malloc(sizeof(setstr_obj_t));
+		if (!obj)
 			goto errout;
-		node->str = strings[i];
+		obj->str = strings[i];
 		strings[i] = NULL;
-		h = tommy_strhash_u32(0, node->str);
-		tommy_hashtable_insert(&this->hashtable, &node->h_node,
-		                       node, h);
+		h = tommy_strhash_u32(0, obj->str);
+		tommy_hashtable_insert(&this->hashtable, &obj->h_node,
+		                       obj, h);
 	}
 	if (strings)
 		free(strings);
@@ -73,11 +86,11 @@ errout:
 
 bool
 setstr_contains(setstr_t *this, const char *str) {
-	setstr_node_t *node;
+	if (this->bucket_max == 0)
+		return false;
 
-	node = tommy_hashtable_search(&this->hashtable, compfunc, str,
+	return tommy_hashtable_search(&this->hashtable, setstr_obj_cmp, str,
 	                              tommy_strhash_u32(0, str));
-	return node != NULL;
 }
 
 /*
@@ -85,6 +98,9 @@ setstr_contains(setstr_t *this, const char *str) {
  */
 bool
 setstr_contains3(setstr_t *this, const char *str, const char *scope) {
+	if (this->bucket_max == 0)
+		return false;
+
 	/* TODO May be further optimized by keeping track of whether setstr
 	 * contains no scoped entries, or setstr contains only scoped entries,
 	 * and skipping the respective lookup accordingly */
@@ -109,10 +125,10 @@ setstr_size(setstr_t *this) {
  */
 void
 setstr_destroy(setstr_t *this) {
-	if (this->bucket_max == 0)
-		return;
-	tommy_hashtable_foreach(&this->hashtable, free);
-	tommy_hashtable_done(&this->hashtable);
+	if (this->bucket_max != 0) {
+		tommy_hashtable_foreach(&this->hashtable, setstr_obj_free);
+		tommy_hashtable_done(&this->hashtable);
+	}
 	bzero(this, sizeof(setstr_t));
 }
 
--- tommy_ext.h
+++ tommy_ext.h
@@ -16,16 +16,23 @@
  * xnumon (OSL 3.0) and TommyDS (2-Clause BSD).
  */
 
+#include "minmax.h"
+
 #include "tommytypes.h"
 
 /* go for 75% of next power of two to stay clear of hashtable
  * performance drop but also avoiding overmuch slack space */
 static inline size_t
 bucket_max_for_buckets(size_t buckets) {
-	size_t bmax = (tommy_roundup_pow2_u32(buckets) >> 2) * 3;
+	size_t bmax;
+
+	if (buckets == 0)
+		return 0;
+
+	bmax = (tommy_roundup_pow2_u32(buckets) >> 2) * 3;
 	if (buckets > bmax)
 		bmax <<= 1;
-	return bmax;
+	return max(bmax, (size_t)16);
 }
 
 #endif
